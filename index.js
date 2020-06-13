afaaa5ae22ba61f4cd52aa189907656a4dd9f211
shivam agrawal <42748556+shivam1192@users.noreply.github.com>
2020-06-13 17:12:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ const checkToken = require('./functions/checkToken');
 const {User , validateUser} = require('./models/user');
 require('express-async-errors');
 
-// mongoose.Promise = global.Promise;
-// // mongoose.connect('mongodb+srv://shivam1192:9827252555sg@cluster0-jicbh.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true},(errors)=>{
-//     if(errors){
-//         console.log(errors)
-//     }
-//     else{
-//         console.log("mongoose ready")
-//     }
-// });
-
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
